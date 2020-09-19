78eab9b6d974d6a7bb7ebd834d2c3959bbe6735b
test <test@example.com>
2020-09-20 09:19:55 +1000

    Added Debounce utility function

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hi there!');
+
+const input = document.querySelector('input');
+
+const fetchData = async(searchTerm) => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params : {
+            apikey: '56ca05c8',
+            s: searchTerm
+        }
+    });
+    console.log(response.data);
+}
+
+
+const onInput = event => {
+    fetchData(event.target.value);
+}
+
+
+input.addEventListener('input', debounce(onInput));
+
