6431ec55c9d4ab06d85ad6148376f607947fb631
test <test@example.com>
2020-09-24 05:01:27 +1000

    Moved HTML creation using js

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-
-const input = document.querySelector('input');
-
-const fetchData = async(searchTerm) => {
-    const response = await axios.get("http://www.omdbapi.com/", {
-        params : {
-            apikey: '56ca05c8',
-            s: searchTerm
-        }
+const fetchData = async searchTerm => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'd9835cc5',
+        s: searchTerm
+      }
     });
-    if(response.data.Error) {
-        return [];
+  
+    if (response.data.Error) {
+      return [];
     }
-   return response.data.Search;
-}
-
-
-const onInput = async event => {
+  
+    return response.data.Search;
+  };
+  
+  const root = document.querySelector('.autocomplete');
+  root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+  `;
+  
+  const input = document.querySelector('input');
+  const dropdown = document.querySelector('.dropdown');
+  const resultsWrapper = document.querySelector('.results');
+  
+  const onInput = async event => {
     const movies = await fetchData(event.target.value);
-    const targetDiv = document.getElementById('target');
-    for(movie of movies) {
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <img src="${movie.Poster}"/>
-        `;
-        targetDiv.appendChild(div);
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
+    for (let movie of movies) {
+      const option = document.createElement('a');
+      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+      option.classList.add('dropdown-item');
+      option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
+      `;
+  
+      resultsWrapper.appendChild(option);
     }
-}
-
-
-input.addEventListener('input', debounce(onInput));
-
+  };
+  input.addEventListener('input', debounce(onInput, 500));
+  
